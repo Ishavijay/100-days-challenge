7abf9e155b59d71a61d20db747935a90782a7587
Ishavijay <ishavijay003@gmail.com>
2025-08-27 23:25:56 +0530

    Add files via upload

---
+++ q3.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    int l=4;
+    int b=12;
+int perimeter=2*(l+b);
+int area=l*b;
+    printf("the area and perimeter of rectamgle with l=4.0 units and b=12.0 units\n");
+    printf("Perimeter = %d\n",perimeter);
+    printf("Area = %d \n",area);
+    return 0;
+}
