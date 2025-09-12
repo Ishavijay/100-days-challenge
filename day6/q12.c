cf7a0a8bd2b2bfba67f7ccdd68d618300adf9d35
Ishavijay <ishavijay003@gmail.com>
2025-09-12 23:32:37 +0530

    Add files via upload

---
+++ day6/q12.c
@@ -0,0 +1,17 @@
+ #include <stdio.h>
+ int main(){
+ int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if(n>0){
+        printf("It is a positive number");
+    }
+    else if(n<0){
+        printf("It is a negative number");
+    }
+    else{
+        printf("THe number is zero");
+    }
+    getchar();
+    return 0;
+}
