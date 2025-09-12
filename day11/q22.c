f49fe7597246b517a3f3f33733ee12157d9dbd7b
Ishavijay <ishavijay003@gmail.com>
2025-09-12 23:57:59 +0530

    Update q22.c

--- day11/q22.c
+++ day11/q22.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 
-int main(){
-    int cp,sp;
-    printf("Enter the selling price:");
-    scanf("%d",&cp);
-    printf("Enter the selling price:");
-    scanf("%d",&sp);
+int main() {
+    int cp, sp;
+    printf("Enter the cost price: ");
+    scanf("%d", &cp);
+    printf("Enter the selling price: ");
+    scanf("%d", &sp);
+
+    if (sp > cp) {
+        printf("It is a profit\n");
+        int profit = sp - cp;
+        float pp = ((float)profit / cp) * 100;
+        printf("The profit percentage is: %.2f%%\n", pp);
+    } 
+    else {
+        printf("It is a loss\n");
+        int loss = cp - sp;
+        float lp = ((float)loss / cp) * 100;
+        printf("The loss percentage is: %.2f%%\n", lp);
+    }
+
+    return 0;
 }
