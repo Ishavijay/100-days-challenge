d2c87a8bce052b790e8d79cd77850bb5db9b97b1
Ishavijay <ishavijay003@gmail.com>
2025-09-12 23:33:21 +0530

    Add files via upload

---
+++ day7/q14.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    char alp;
+    printf("Enter any alphabet:");
+    scanf("%c",&alp);
+    if(alp == 'A'||alp == 'E'||alp == 'I'||alp == 'O'||alp == 'U'||alp == 'a'||alp == 'e'||alp == 'i'||alp == 'o'||alp == 'u'){
+         printf("%c is a vowel!!",alp);
+    }
+    else{
+        printf("%c is a consonant!!",alp);
+    }
+    return 0;
+    
+}
