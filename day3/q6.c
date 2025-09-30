20701e615aa3bc277296e0d495e8daed8170e12e
Ishavijay <ishavijay003@gmail.com>
2025-09-30 20:37:47 +0530

    Update q6.c

--- day3/q6.c
+++ day3/q6.c
@@ -1,3 +1,4 @@
+//Write a program to swap two numbers using a third variable.
 #include <stdio.h>
 
 int main(){
@@ -10,4 +11,5 @@ int main(){
     printf("Previous value of a was 3 but after swapping it is %d \n",a);
     printf("Previous value of b was 6 but after swapping it is %d",b);
     return 0;
+
 }
