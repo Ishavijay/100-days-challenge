a54a23bbe2ad07adce2ee414743ecba261908dcf
Ishavijay <ishavijay003@gmail.com>
2025-10-05 20:20:07 +0530

    Add files via upload

---
+++ day25/q50.c
@@ -0,0 +1,20 @@
+/* Write a program to print the following pattern:
+*****
+ ****
+  ***
+   **
+    * */
+
+    #include <stdio.h>
+
+    int main(){
+
+        for(int i=1;i<=5;i++){
+
+            for(int j=5;j<=1;j++){
+                printf("*");
+            }
+            printf("\n");
+        }
+        return 0;
+    }
