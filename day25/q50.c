6b00fe15971eb1455739e4ee4cc71a2241e8cbe0
Ishavijay <ishavijay003@gmail.com>
2025-10-05 20:22:21 +0530

    Update q50.c

--- day25/q50.c
+++ day25/q50.c
@@ -5,16 +5,20 @@
    **
     * */
 
-    #include <stdio.h>
+   #include <stdio.h>
 
-    int main(){
-
-        for(int i=1;i<=5;i++){
-
-            for(int j=5;j<=1;j++){
-                printf("*");
-            }
-            printf("\n");
+int main() {
+    int n = 5; 
+    for(int i = 1; i <= n; i++) {
+       
+        for(int j = 1; j < i; j++) {
+            printf(" ");
         }
-        return 0;
+       
+        for(int j = n; j >= i; j--) {
+            printf("*");
+        }
+        printf("\n");
     }
+    return 0;
+}
