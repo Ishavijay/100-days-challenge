d2b54d04b9b59309e3f1826a167b0445162026bc
Ishavijay <ishavijay003@gmail.com>
2025-11-30 11:13:05 +0530

    Update q142.c

--- day92/q142.c
+++ day92/q142.c
@@ -2,37 +2,34 @@
 #include <stdio.h>
 
 struct Student {
-    int id;
+    int rno;
     char name[50];
     float marks;
 };
 
 int main() {
-    struct Student s[5];
-    int i;
+    struct Student s[6];
+    int j;
+    for (j = 0; j < 6; j++) {
+        printf("\nEnter details for Student %d:\n", j + 1);
 
-    // Input details of 5 students
-    for (i = 0; i < 5; i++) {
-        printf("\nEnter details for Student %d:\n", i + 1);
-
-        printf("ID: ");
-        scanf("%d", &s[i].id);
+        printf("Student roll no: ");
+        scanf("%d", &s[j].rno);
 
         printf("Name: ");
-        scanf("%s", s[i].name);
+        scanf("%s", s[j].name);
 
         printf("Marks: ");
-        scanf("%f", &s[i].marks);
+        scanf("%f", &s[j].marks);
     }
 
     // Print all students' details
-    printf("\n--- All Student Details ---\n");
-    for (i = 0; i < 5; i++) {
-        printf("\nStudent %d:\n", i + 1);
-        printf("ID    : %d\n", s[i].id);
-        printf("Name  : %s\n", s[i].name);
-        printf("Marks : %.2f\n", s[i].marks);
+    printf("\n-------------------------Student details ---------------------------\n");
+    for (j = 0; j < 6; j++) {
+        printf("\nStudent %d:\n", j + 1);
+        printf("Student roll no : %d\n", s[j].rno);
+        printf("Name  : %s\n", s[j].name);
+        printf("Marks : %.2f\n", s[j].marks);
     }
+    return 0;}
 
-    return 0;
-}
