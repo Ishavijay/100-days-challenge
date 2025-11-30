615c271e22254a1eb9f059a373d889908cc0680f
Ishavijay <ishavijay003@gmail.com>
2025-11-30 12:25:24 +0530

    Update q127.c

--- day77/q127.c
+++ day77/q127.c
@@ -1,36 +1,30 @@
-//Write a program that reads text from input.txt, converts all lowercase letters to uppercase, and writes the result to output.txt.
+/*Write a program that reads text from input.txt, converts all lowercase letters to uppercase
+,and writes the result to output.txt.*/
 #include <stdio.h>
-#include <ctype.h>   // for toupper()
-
+#include <ctype.h> 
 int main() {
     FILE *fin, *fout;
-    char ch;
+    char c;
 
     fin = fopen("input.txt", "r");
     if (fin == NULL) {
         printf("Error opening input.txt\n");
         return 1;
     }
-
     fout = fopen("output.txt", "w");
     if (fout == NULL) {
         printf("Error opening output.txt\n");
         fclose(fin);
         return 1;
     }
-
-    // Read each character
-    while ((ch = fgetc(fin)) != EOF) {
-        if (ch >= 'a' && ch <= 'z')
-            ch = toupper(ch);   // convert to uppercase
-
-        fputc(ch, fout);        // write to output file
+    while ((c = fgetc(fin)) != EOF) {
+        if (c >= 'a' && c <= 'z')
+            c = toupper(c);  
+        fputc(c, fout);        
     }
-
     fclose(fin);
     fclose(fout);
-
     printf("Conversion complete! Check output.txt\n");
-
     return 0;
 }
+
