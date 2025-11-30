d71170f67c674b6ed084bb57cfa8d46069aa190f
Ishavijay <ishavijay003@gmail.com>
2025-11-30 12:35:30 +0530

    Update q120.c

--- day70/q120.c
+++ day70/q120.c
@@ -2,29 +2,28 @@
 
 #include <stdio.h>
 int main() {
- 
-    char str[200];
-    printf("Enter a string: ");
-    gets(str);  
-
-    int i = 0;
+    char string[200];
+    printf("Enter The string: ");
+    gets(string);  
+    int j = 0;
     int newWord = 1;  
-    while(str[i] != '\0'){
-        if(str[i] == ' '){
+    while(string[j] != '\0'){
+        if(string[j] == ' '){
             newWord = 1;  
         }else{
-            if(newWord && str[i] >= 'a' && str[i] <= 'z'){
-                str[i] = str[i] - 32;  
+            if(newWord && string[j] >= 'a' && string[j] <= 'z'){
+                string[j] = string[i] - 32;  
                 newWord = 0;
-            }else if(!newWord && str[i] >= 'A' && str[i] <= 'Z'){
-                str[i] = str[i] + 32;  
+            }else if(!newWord && string[j] >= 'A' && string[j] <= 'Z'){
+                string[j] = string[j] + 32;  
             }else{
                 newWord = 0;  
             }
         }
-        i++;
+        j++;
     }
 
-    printf("Sentence Case: %s\n", str);
+    printf("Sentence Case: %s\n", string);
     return 0;
+
 }
