ebee031ab0ba5da04dfc2f6ea8aa02181577c4a9
Ishavijay <ishavijay003@gmail.com>
2025-11-30 12:38:30 +0530

    Update q119.c

--- day69/q119.c
+++ day69/q119.c
@@ -1,26 +1,24 @@
 /* Q119: Write a program to take an integer array as input. Only one element will be repeated. 
 Print the repeated element. Try to find the result in one single iteration. */
-
 #include <stdio.h>
 int main() {
-
-    int n;
+    int k;
     printf("Enter size of array: ");
-    scanf("%d", &n);
+    scanf("%d", &k);
 
-    int a[n], visited[1000] = {0};
+    int a[k], visited[1000] = {0};
 
     printf("Enter elements: ");
-    for(int i = 0; i < n; i++){
-        scanf("%d", &a[i]);
+    for(int j = 0; j < k; j++){
+        scanf("%d", &a[j]);
 
-        if(visited[a[i]] == 1){
-            printf("Repeated element: %d\n", a[i]);
+        if(visited[a[j]] == 1){
+            printf("Repeated element: %d\n", a[j]);
             return 0;
         }
-        visited[a[i]] = 1;
+        visited[a[j]] = 1;
     }
-
     printf("No repeated element found.\n");
     return 0;
+
 }
