d870bd45b5dcc8c4aab84bd9377307ec21e614d6
Ishavijay <ishavijay003@gmail.com>
2025-11-30 10:54:49 +0530

    Update q143.c

--- day93/q143.c
+++ day93/q143.c
@@ -2,44 +2,40 @@
 #include <stdio.h>
 
 struct Student {
-    int id;
-    char name[50];
+    int student_id;
+    char student_name[50];
     float marks;
 };
 
 int main() {
-    int n, i, topIndex = 0;
+    int k,j, tIndex = 0;
 
     printf("Enter number of students: ");
-    scanf("%d", &n);
+    scanf("%d", &k);
 
-    struct Student s[n];
+    struct Student s[k];
 
     // Input student data
-    for (i = 0; i < n; i++) {
-        printf("\nEnter details of Student %d:\n", i + 1);
-        printf("ID: ");
-        scanf("%d", &s[i].id);
+    for (j = 0; i < k; j++) {
+        printf("\nEnter details of Student %d:\n", j + 1);
+        printf("Student ID: ");
+        scanf("%d", &s[j].student_id);
 
         printf("Name: ");
-        scanf("%s", s[i].name);
+        scanf("%s", s[j].student_name);
 
         printf("Marks: ");
-        scanf("%f", &s[i].marks);
+        scanf("%f", &s[j].marks);
     }
-
-    // Find highest marks
-    for (i = 1; i < n; i++) {
-        if (s[i].marks > s[topIndex].marks) {
-            topIndex = i;
-        }
-    }
-
-    // Print top student
-    printf("\n--- Student with Highest Marks ---\n");
-    printf("ID    : %d\n", s[topIndex].id);
-    printf("Name  : %s\n", s[topIndex].name);
-    printf("Marks : %.2f\n", s[topIndex].marks);
-
+    for (j = 1; i < k; j++) {
+        if (s[j].marks > s[tIndex].marks) {
+            tIndex = j;
+        } }
+    printf("\n--- Details of student who scored highest marks!! ---\n");
+    printf("Student ID  : %d\n", s[tIndex].student_id);
+    printf("Name  : %s\n", s[tIndex].student_name);
+    printf("Marks : %.2f\n", s[tIndex].marks);
+    
     return 0;
 }
+
