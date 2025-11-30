1ed0ae7aa654850481356d7a6a5315e672344754
Ishavijay <ishavijay003@gmail.com>
2025-11-30 12:06:19 +0530

    Update q123.c

--- day73/q123.c
+++ day73/q123.c
@@ -1,43 +1,37 @@
-//Read a text file and count the total number of characters, words, and lines. A word is defined as a sequence of non-space characters separated by spaces or newlines.
+/*Read a text file and count the total number of characters, words, and lines. 
+A word is defined as a sequence of non-space characters separated by spaces or newlines.*/
 #include <stdio.h>
 #include <ctype.h>
-
 int main() {
     FILE *fp;
-    char filename[100];
-    char ch;
-    int charCount = 0, wordCount = 0, lineCount = 0;
-    int inWord = 0;
+    char File_name[100];
+    char C;
+    int cCount= 0, wCount = 0, lCount = 0;
+    int iWord = 0;
 
     printf("Enter filename: ");
-    scanf("%s", filename);
+    scanf("%s", File_name);
 
     fp = fopen(filename, "r");
     if (fp == NULL) {
-        printf("Error opening file '%s'\n", filename);
+        printf("Error opening file '%s'\n", File_name);
         return 1;
     }
-
-    while ((ch = fgetc(fp)) != EOF) {
-        charCount++;  // count every character
-
-        if (ch == '\n') {
-            lineCount++;  // count lines
-        }
-
-        if (isspace(ch)) {
-            inWord = 0;   // outside a word
+    while ((C = fgetc(fp)) != EOF) {
+        cCount++; 
+        if (C == '\n') {
+            lCount++; }
+        if (isspace(C)) {
+            iWord = 0; 
         } else if (inWord == 0) {
-            inWord = 1;   // starting a new word
-            wordCount++;
+            iWord = 1;  
+            wCount++;
         }
-    }
-
-    fclose(fp);
-
-    printf("\nTotal Characters: %d\n", charCount);
-    printf("Total Words     : %d\n", wordCount);
-    printf("Total Lines     : %d\n", lineCount);
+    }    fclose(fp);
+printf("\nTotal Characters: %d\n", cCount);
+    printf("Total Words     : %d\n", wCount);
+    printf("Total Lines     : %d\n", lCount);
 
     return 0;
 }
+
