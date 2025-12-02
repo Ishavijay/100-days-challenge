e83c5261b041468d9e84bf4e267ca7de6025c7ce
Ishavijay <ishavijay003@gmail.com>
2025-12-02 22:06:52 +0530

    Update q86.c

--- day43/q86.c
+++ day43/q86.c
@@ -1,2 +1,30 @@
 //Check if a string is a palindrome.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    scanf("%[^\n]", str);
+
+    int start = 0, end = 0;
+
+    // Find length manually
+    while (str[end] != '\0') {
+        end++;
+    }
+    end--; // last valid character
+
+    // Check palindrome
+    while (start < end) {
+        if (str[start] != str[end]) {
+            printf("Not a palindrome\n");
+            return 0;
+        }
+        start++;
+        end--;
+    }
+
+    printf("Palindrome\n");
+    return 0;
+}
+
 
