4d0a2ac0f9124bee09223778ee107b450e420e5a
Ishavijay <ishavijay003@gmail.com>
2025-12-02 22:06:19 +0530

    Update q85.c

--- day43/q85.c
+++ day43/q85.c
@@ -1,1 +1,30 @@
 //Reverse a string.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    scanf("%[^\n]", str);
+
+    int start = 0;
+    int end = 0;
+
+    // Find length manually
+    while (str[end] != '\0') {
+        end++;
+    }
+    end--;  // Move to last valid character
+
+    // Reverse using two-pointer technique
+    while (start < end) {
+        char temp = str[start];
+        str[start] = str[end];
+        str[end] = temp;
+
+        start++;
+        end--;
+    }
+
+    printf("%s\n", str);
+
+    return 0;
+}
